1953af8cecdd990ca6a226c06340caf6038c9986
Muhammad Thariq Darobi <93000227+muhammad-thariq@users.noreply.github.com>
2024-09-27 15:53:47 +0700

    Add files via upload

---
+++ quiz1/script.js
@@ -0,0 +1,10 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//     // You can dynamically set the image source here if needed
+//     document.getElementById('image').src = 'elements/IMG_4921.jpg';
+// });
+
+const video = document.getElementById('video');
+video.playbackRate = 2.0;
+
+
+setInterval(updateContent, 8000);
